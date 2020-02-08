7dc2222366586a6fddd4685036613db52d5b70e5
Ashfaq Rahman <ashfaq.headrun@embibe.com>
2020-02-08 11:44:15 +0530

    datetime parser: implemented: unit tested

--- utils.py
+++ utils.py
@@ -1,24 +1,42 @@
 import datetime
 from dateutil import parser as date_parser
 
-verbose_days = {'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun' }
+verbose_days = [ 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun' ]
 
 def make_EOD(d):
     return d.replace(hour=20, minute=00, second=00, microsecond=0)
 
+def int_or_zero(t):
+    try:
+        return int(t)
+    except ValueError:
+        return 0
+
+def validate_hour(h):
+    h = abs(int(h))
+    if h > 24:
+        h = 23
+    return h
+
+def validate_minute(m):
+    m = abs(int(m))
+    if m > 60:
+       m = 59
+    return m
+
 def verbose_day_to_datetime(verbose):
     td_1 = datetime.timedelta(days=1)
     if verbose == 'today':
-        parsed_date = date_parser.parse(verbose)
+        parsed_date = datetime.datetime.now()
     elif verbose == 'tomorrow':
         parsed_date = datetime.datetime.now() + td_1
     elif verbose in verbose_days:
-        parsed_date = parser.parse(verbose)
+        parsed_date = date_parser.parse(verbose)
     else:
         raise Exception(verbose + ' is not a supported format. \nFormats supported: %s' % verbose_days)
     return make_EOD(parsed_date)
 
-def parse_date(unparsed_date):
+def parse_datetime(unparsed_datetime):
     '''
     unparsed_date:
         days:
@@ -32,23 +50,54 @@ def parse_date(unparsed_date):
         1. If you only want the hours needed, then follow the same format as <time> but with a hyphenated 'r'
         Ex. 5:00-r (Task can be completed in 5 hours or deadline is in 5 hours)
     '''
+    if unparsed_datetime.count('-') > 1 or unparsed_datetime.count(':') > 1:
+        raise Exception('Unsupported format %s' % unparsed_datetime)
 
-    if '-' in unparsed_date:
-        if 'today' in unparsed_date or 'tomorrow' in unparsed_date:
-            #create a time from the hyphenated integer
-            day, time = unparsed_date.split('-')
-            try:
-                hour, minute = time.split(':')
-            except ValueError:
-                hour = time
-        elif 'tomorrow' in unparsed_date:
+    if '-' in unparsed_datetime:
+        try:
+            day, time = unparsed_datetime.split('-')
+        except ValueError:
+            raise Exception('Unsupported format: %s' % unparsed_datetime)
+
+        #parse time
+        try:
+            hour, minute = map(int_or_zero, time.split(':'))
+        except ValueError:
+            hour, minute = map(int_or_zero, time.split(':') + [0])
+        
+        hour, minute = validate_hour(hour), validate_minute(minute)
+        
+        #parse day
+        parsed_day = verbose_day_to_datetime(day)
+        parsed_datetime = parsed_day.replace(hour=hour, minute=minute)
     else:
-        parsed_date = verbose_day_to_datetime(unparsed_date)
+        #parse hour
+        if ':' not in unparsed_datetime:
+            try:
+                hour = int(unparsed_datetime)
+                hour = validate_hour(hour)
+                parsed_datetime = datetime.timedelta(hours=hour)
+            except ValueError:
+                parsed_datetime = verbose_day_to_datetime(unparsed_datetime)
+        #parse hour with minutes
+        else:
+            hour, minute = map(int_or_zero, unparsed_datetime.split(':'))
+            hour, minute = validate_hour(hour), validate_minute(minute)
+            parsed_datetime = datetime.timedelta(hours=hour, minutes=minute)
+    return parsed_datetime
 
 
 if __name__ == '__main__':
-    pass
-    #TODO unit-testing
+    test_cases = ['12:30', '12', ':30', '12:', 'hi', 'bro',
+                '2313', '0x232', 'mon-2', 'mon-12:30',
+                'sun-12:5423', '12:232323', 'tue--2', '12::30'
+                ] + verbose_days
+    for test_case in test_cases:
+        try:
+            print(parse_datetime(test_case))
+        except Exception as e:
+            print(e)
+
 
 
 
